cde20e085564b1a99f3c4aa605cd5d88b0169b8a
“Shaun <shaunwmusic@gmail.com>
2016-11-06 13:05:44 -0600

    Static HTML Application component

--- app/routes/application.vue
+++ app/routes/application.vue
@@ -1,14 +1,24 @@
 <template lang="html">
   <div class="">
-
+    <div class="panel">
+    <div class="panel-heading">
+        <div class="panel-block">
+        <form class="" action="index.html" method="post">
+          <input type="button" name="name" value="">
+        </form>
+        <div class="panel-block">
+          <button></button>
+        </div>
+      </div>
+    </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {
-    };
+    return {};
   },
 
   methods: {
