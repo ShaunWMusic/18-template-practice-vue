0bc4fab03721b2fe448128f8ba9bd88f5548b97d
“Shaun <shaunwmusic@gmail.com>
2016-11-07 16:51:27 -0600

    Working on for loop

--- app/routes/application.vue
+++ app/routes/application.vue
@@ -1,74 +1,73 @@
 <template lang="html">
   <div class="">
 <div class="section page">
-<nav class="panel">
+<form class="panel">
   <div class="panel-heading">
     <h2>Sign Up For My Web App</h2>
     </div>
     <div class="panel-block">
-      <label class="control has-icon">
-        <input class="input" type="user" placeholder="First Name">
-          <span class="fa fa-user"></span>
-        </input>
-      </label>
-    </div>
-    <div class="panel-block">
-      <p class="control has-icon">
-        <input class="input" type="user" placeholder="Last Name">
-            <span class="fa fa-user"></span>
-        </input>
-      </p>
-    </div>
-    <div class="panel-block">
-      <p class="control has-icon">
-        <input class="input" type="email" placeholder="Email Address">
-            <span class="fa fa-envelope"></span>
-        </input>
-      </p>
-    </div>
-    <div class="panel-block">
-      <p class="control has-icon">
-        <input class="input" type="website" placeholder="Current website url">
-            <span class="fa fa-globe"></span>
-        </input>
-      </p>
-    </div>
+      <p class="control has-icon" v-for="formInput in formInputs">
+      <template v-if="formInput.type === 'select'" v-model="formValues[formInput.id]">
+          <span class="select is-fullwidth">
+            <select>
+              <option v-for="option in formInput.options">
+                {{ option.label }}
+              </option>
+            </select>
+          </span>
+      </template>
+
+      <template v-else>
+        <template v-if="formInput.type === 'textarea'" v-model="formValues[formInput.id]">
+          <textarea class="textarea input" type="textarea" :placeholder="formInput.label">
+          </textarea>
+          <i :class="formInput.icon" class="fa" aria-hidden="true"></i>
+        </template>
+        <template v-else>
+          <input class="input" :placeholder="formInput.label" v-model="formValues[input.id]">
+          <i :class="formInput.icon" class="fa" aria-hidden="true"></i>
+        </template>
+      </template>
+
+
+
+    <!-- <p class="control has-icon" v-for="data in formInputs">
+      <input class="input" type="text" :placeholder="data.label">
+      <span class="fa" :class="data.icon"></span>
+    </p>
+
       <div class="panel-block">
-        <p class="control">
-            <span class="select is-fullwidth">
-        <select>
-          <option>English</option>
-          <option>Spanish</option>
-        </select>
-            </span>
-        </p>
+      <template v-else="ok">
+      </template>
       </div>
       <div class="panel-block">
-        <p class="control has-icon">
+        <label class="control has-icon">
           <span class="fa fa-comments"></span>
-            <textarea class="textarea input" type="textarea" placeholder="Your Comment">
+            <textarea class="textarea input" type="textarea" :placeholder="formInputs[5].label">
             </textarea>
-        </p>
+        </label>
       </div>
       <div class="panel-block">
-        <p class="control has-icon">
+        <label class="control has-icon">
           <input class="input" type="mobile" placeholder="Mobile Number">
               <span class="fa fa-mobile"></span>
           </input>
-        </p>
+        </label>
       </div>
       <div class="panel-block">
-        <p class="control has-icon">
+        <label class="control has-icon">
           <input class="input" type="tel" placeholder="Home Number">
               <span class="fa fa-phone"></span>
           </input>
-        </p>
-      </div>
-      <p class="control">
+        </label>
+      </div> -->
+      </p>
+    </div>
+      <div class="panel-block">
     <button class="button is-primary is-fullwidth">Submit</button>
     <button class="button is-fullwidth">Cancel</button>
-      </p>
-      </nav>
+  </div>
+    </form>
       </div>
   </div>
 </template>
@@ -83,6 +82,10 @@ export default {
       formInputs: [],
       formValues: [],
     };
+  },
+
+  mounted() {
+    this.getData();
   },
 
   methods: {
