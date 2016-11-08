390827d58f64401905b006cd1bec5825434eb8fb
“Shaun <shaunwmusic@gmail.com>
2016-11-07 18:52:14 -0600

    Finish form. Ready to be reviewed

--- app/routes/application.vue
+++ app/routes/application.vue
@@ -1,7 +1,7 @@
 <template lang="html">
   <div class="">
 <div class="section page">
-<form class="panel">
+<form @submit.prevent="submitForm" class="panel">
   <div class="panel-heading">
     <h2>Sign Up For My Web App</h2>
     </div>
@@ -19,56 +19,22 @@
       </template>
 
       <template v-else>
-        <template v-if="formInput.type === 'textarea'">
+<template v-if="formInput.type === 'textarea'">
           <textarea class="textarea input"
             type="textarea"
             :placeholder="formInput.label"
             v-model="formValues[formInput.id]"></textarea>
           <i :class="formInput.icon" class="fa" aria-hidden="true"></i>
-        </template>
+</template>
         <template v-else>
-          <input class="input" :placeholder="formInput.label" v-model="formValues[formInput.id]">
-          <i :class="formInput.icon" class="fa" aria-hidden="true"></i>
-        </template>
+<input class="input" :placeholder="formInput.label" v-model="formValues[formInput.id]">
+<i :class="formInput.icon" class="fa" aria-hidden="true"></i>
+</template>
       </template>
-
-
-
-    <!-- <p class="control has-icon" v-for="data in formInputs">
-      <input class="input" type="text" :placeholder="data.label">
-      <span class="fa" :class="data.icon"></span>
-    </p>
-
-      <div class="panel-block">
-      <template v-else="ok">
-      </template>
-      </div>
-      <div class="panel-block">
-        <label class="control has-icon">
-          <span class="fa fa-comments"></span>
-            <textarea class="textarea input" type="textarea" :placeholder="formInputs[5].label">
-            </textarea>
-        </label>
-      </div>
-      <div class="panel-block">
-        <label class="control has-icon">
-          <input class="input" type="mobile" placeholder="Mobile Number">
-              <span class="fa fa-mobile"></span>
-          </input>
-        </label>
-      </div>
-      <div class="panel-block">
-        <label class="control has-icon">
-          <input class="input" type="tel" placeholder="Home Number">
-              <span class="fa fa-phone"></span>
-          </input>
-        </label>
-      </div> -->
       </p>
     </div>
       <div class="panel-block">
     <button class="button is-primary is-fullwidth">Submit</button>
-    <button class="button is-fullwidth">Cancel</button>
   </div>
     </form>
       </div>
@@ -92,17 +58,28 @@ export default {
   },
 
   methods: {
-    getData () {
+    getData() {
       fetch(apiUrl)
         .then((r) => r.json())
         .then((formInputs) => {
           console.log(formInputs);
           this.formInputs = formInputs;
+          console.log(formInputs);
         });
-      },
+    },
 
-    submitForm() {},
+    submitForm(formValues) {
+      fetch('http://tiny-tn.herokuapp.com/collections/form-shaun', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(this.formValues)
+      });
 
   },
+
+},
 };
 </script>
